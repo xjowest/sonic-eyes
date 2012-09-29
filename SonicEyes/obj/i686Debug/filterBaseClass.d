95e23f2796b2468879fe14b7be2533ceca3805eb
Petter Grundström <petgr138@gmail.com>
2012-09-29 14:37:37 +0200

    Removed unnecessary files. Only source files should be pushed

--- SonicEyes/obj/i686Debug/filterBaseClass.d
+++
@@ -1,6 +0,0 @@
-obj/i686Debug/./filterBaseClass.d obj/i686Debug/./filterBaseClass.o: \
- filterBaseClass.cc filterBaseClass.h typeDefs.h
-
-filterBaseClass.h:
-
-typeDefs.h:
--- SonicEyes/obj/i686Debug/highPassFilter.d
+++
@@ -1,8 +0,0 @@
-obj/i686Debug/./highPassFilter.d obj/i686Debug/./highPassFilter.o: \
- highPassFilter.cc highPassFilter.h filterBaseClass.h typeDefs.h
-
-highPassFilter.h:
-
-filterBaseClass.h:
-
-typeDefs.h:
