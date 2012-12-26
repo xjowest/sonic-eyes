5f29ccd8a4bf6e138e9d39b182117db0279b87c2
Johan Westlund <johanwestlund_100@hotmail.com>
2012-12-26 18:02:11 +0100

    Se förra commit

--- SonicEyes/src/soundCreator.cc
+++ SonicEyes/src/soundCreator.cc
@@ -7,37 +7,14 @@ soundCreator::~soundCreator() {
 }
 
 void soundCreator::createSound(SampleBurst * sampleBurst, ClusterData & cluster) {
-    for(int i = 0; i < (int)cluster.clusters.size(); i++) {	
-	for(int j = cluster.clusters[i].startIndex; j <= cluster.clusters[i].endIndex; j++){ // j+ 100 = frekvens i Hz
-	    for(int t = 0; t < NUMBER_OF_SOUND_SAMPLES; t++) {
-		float desiredFrequency = j+100;
-		sampleBurst->burst[t] += (int)(28000 - (cluster.clusters[i].depth * 18000 / 5)) * (sin((t * desiredFrequency * 2 * M_PI) / SAMPLE_FREQUENCY / 2));
-	    }
+    for(int i = 0; i < (int)cluster.clusters.size(); i++) {
+		if(cluster.clusters[i].endIndex - cluster.clusters[i].startIndex > 20){
+			int frequency = cluster.clusters[i].startIndex * 10 + 500;
+			for(int t=0;t<NUMBER_OF_SOUND_SAMPLES;t++){
+				sampleBurst->burst[t] += (4000 - (cluster.clusters[i].depth * 2000 / 5) / cluster.clusters.size()) * sin(2 * M_PI * frequency * t / SAMPLE_FREQUENCY);
+			}
+			
+			printf("Playing frequencies: %i\n", frequency);
+    	}
 	}
-    }
-
-    int highestAmplitude = 0;
-    for(int t = 0; t < NUMBER_OF_SOUND_SAMPLES; t++) {
-	if(sampleBurst->burst[t] > highestAmplitude) {
-	    highestAmplitude = sampleBurst->burst[t];
-	}
-    }
-
-    for(int t = 0; t < NUMBER_OF_SOUND_SAMPLES; t++) {
-	//printf("Burst value %i\n", sampleBurst->burst[t]);
-	//printf("Highest value %i\n", highestAmplitude);
-	sampleBurst->burst[t] = (int)((float)sampleBurst->burst[t] / (float)highestAmplitude * 28000);
-	//printf("Burst normalized %i\n", sampleBurst->burst[t]);
-    }
-//    float desiredAmplitude = 28000.0 - (cluster.clusters[i].depth * 18000 / 5.0);
-//    sampleBurst->burst[t] = sampleBurst->burst[t] * desiredAmplitude;
-/*    
-      for(int i = 0; i < NUMBER_OF_SOUND_SAMPLES; i++) {
-      float desiredFrequency = 500;
-      sampleBurst->burst[i] = (int)(25000 * sin((i * desiredFrequency * 2 * M_PI) / SAMPLE_FREQUENCY / 2));
-      printf("sample[%i]:%i\n",i,sampleBurst->burst[i]);
-      printf("realValue:%f\n", (i * desiredFrequency) / SAMPLE_FREQUENCY / 2);
-      printf("desired:%f\n",desiredFrequency);
-      }
-*/
 }
